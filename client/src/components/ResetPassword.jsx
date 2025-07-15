24f7f20a4ace9cdf59e38963fcd45350384d65dc
Blackeye994 <msamad09874821@gmail.com>
2025-07-15 23:34:18 +0500

    first commit

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -30,7 +30,7 @@ export default function ResetPassword() {
     try {
       setLoading(true);
 
-      const res = await fetch(`http://localhost:5000/api/auth/reset-password/${token}`, {
+      const res = await fetch(`https://doctor-appointment-system-sage-psi.vercel.app/api/auth/reset-password/${token}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,18 +10,12 @@ export default defineConfig({
   ],
   server: {
     proxy: {
-      '/api/auth/register': 'http://localhost:5000',
-      '/api/auth/login': 'http://localhost:5000',
-      '/api/doctors': 'http://localhost:5000',
-      '/api/doctors/register/:doctorId': 'http://localhost:5000',
-      '/api': 'http://localhost:5000',
-      '/api/appointments/:id': 'http://localhost:5000',
-      // '/Auth/forgot-password': 'http://localhost:3000',
-      // '/Auth/reset-password/:token': 'http://localhost:3000',
-      // '/dash/expenses': 'http://localhost:3000',
-      // '/dash/incomes': 'http://localhost:3000',
-      // '/dash/expense/:id': 'http://localhost:3000',
-      // '/dash/incomes/:id': 'http://localhost:3000',
+      '/api/auth/register': 'https://doctor-appointment-system-sage-psi.vercel.app',
+      '/api/auth/login': 'https://doctor-appointment-system-sage-psi.vercel.app',
+      '/api/doctors': 'https://doctor-appointment-system-sage-psi.vercel.app',
+      '/api/doctors/register/:doctorId': 'https://doctor-appointment-system-sage-psi.vercel.app',
+      '/api': 'https://doctor-appointment-system-sage-psi.vercel.app',
+      '/api/appointments/:id': 'https://doctor-appointment-system-sage-psi.vercel.app',
     }
   }
 })
