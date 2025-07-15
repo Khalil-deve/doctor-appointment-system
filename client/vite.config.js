f5f25a967284e600116d15971d085e7f819afdcd
Blackeye994 <msamad09874821@gmail.com>
2025-07-15 23:44:21 +0500

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: "/",
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api/auth/register': 'https://doctor-appointment-system-sage-psi.vercel.app',
-      '/api/auth/login': 'https://doctor-appointment-system-sage-psi.vercel.app',
-      '/api/doctors': 'https://doctor-appointment-system-sage-psi.vercel.app',
-      '/api/doctors/register/:doctorId': 'https://doctor-appointment-system-sage-psi.vercel.app',
-      '/api': 'https://doctor-appointment-system-sage-psi.vercel.app',
-      '/api/appointments/:id': 'https://doctor-appointment-system-sage-psi.vercel.app',
-    }
-  }
-})
+      "/api/auth/register":
+        "https://doctor-appointment-system-sage-psi.vercel.app",
+      "/api/auth/login":
+        "https://doctor-appointment-system-sage-psi.vercel.app",
+      "/api/doctors": "https://doctor-appointment-system-sage-psi.vercel.app",
+      "/api/doctors/register/:doctorId":
+        "https://doctor-appointment-system-sage-psi.vercel.app",
+      "/api": "https://doctor-appointment-system-sage-psi.vercel.app",
+      "/api/appointments/:id":
+        "https://doctor-appointment-system-sage-psi.vercel.app",
+    },
+  },
+});
